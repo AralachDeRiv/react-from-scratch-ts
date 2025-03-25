24b9072bf37cbc270b4b095d8bb9abc82b1a5c55
Loki17 <Tkl33pac@proton.me>
2025-03-25 16:44:16 +0100

    some css + font

--- src/mainComponents/header.tsx
+++ src/mainComponents/header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
     let previousColorClass = "";
     let currentColorClass = "";
 
-    const updateLogoColor = () => {
+    const updateLogoColorAndAnimation = () => {
       let closestSection: HTMLElement | null = null;
       let minDistance = Infinity;
 
@@ -57,11 +57,11 @@ export const Header = () => {
       }
     };
 
-    window.addEventListener("scroll", updateLogoColor);
-    updateLogoColor();
+    window.addEventListener("scroll", updateLogoColorAndAnimation);
+    updateLogoColorAndAnimation();
 
     return () => {
-      window.removeEventListener("scroll", updateLogoColor);
+      window.removeEventListener("scroll", updateLogoColorAndAnimation);
     };
   }, []);
 
@@ -73,6 +73,9 @@ export const Header = () => {
 
       <div className="link-container">
         <img src="/github-icon.svg" alt="github icon" />
+        <a href="https://github.com/" target="_blanc">
+          See the code
+        </a>
       </div>
 
       <style>
@@ -103,7 +106,7 @@ export const Header = () => {
             background-color: transparent;
             backdrop-filter: blur(2px);
 
-            border-bottom: 1px solid #827a7a;
+            border-bottom: 1px solid rgba(130, 122, 122, 0.24);
 
             & .logo-container{
                 --scaleAmplitude: 0.2;
@@ -115,7 +118,7 @@ export const Header = () => {
                 & h2{  
                     font-family: "Luckiest Guy", cursive;
                     font-weight: 400;      
-                    transition: color 0.3s linear;
+                    transition: color 0.5s ease-in-out;
 
                     &.black-logo{
                       color: var(--black);
@@ -128,10 +131,46 @@ export const Header = () => {
             }
 
             & .link-container{
+              position: relative;
               background-color: var(--white);
               border: 1px solid var(--white);
               border-radius: 50%;
-              cursor: pointer;
+              
+
+
+              & a{
+                position: absolute;
+                overflow: hidden;
+                top: 0;
+                left: 0;
+                transform: translate(-110%, -3px);
+
+
+                color: var(--black);
+                white-space: nowrap;
+                text-decoration: none;
+
+
+                font-size: 13px;
+                padding: 3px 10px;
+                background-color: var(--white);
+                border: 1px solid var(--black);
+                border-radius: 6px;
+                opacity: 0;
+                transition: opacity 0.6s ease;
+
+     
+              }
+
+
+              &:hover a{
+                opacity: 1;
+            
+              }
+
+
+
+
          
             }          
         }
